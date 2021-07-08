1529525d45548df8aa16bd8e65c1534e33d63041
Vipin Cherian <vipincheriandev@outlook.com>
2021-07-08 20:30:58 +0530

    Controls in the timer frame are now programmatically vertically aligned.

--- src/timerframe.pas
+++ src/timerframe.pas
@@ -610,6 +610,7 @@ end;
 
 procedure TfraTimer.ArrangeControls;
 begin
+  // Horizontally arrange controls
   cbSelect.Left := TIMER_PADDING;
   imgTimer.Left := cbSelect.Left + cbSelect.Width + TIMER_PADDING;
   edtTitle.Left := imgTimer.Left + imgTimer.Width + TIMER_PADDING;
@@ -623,6 +624,20 @@ begin
   bbPlay.Left := bbPause.Left - bbPlay.Width - TIMER_PADDING;
   dtpSet.Left := bbPlay.Left - dtpSet.Width - TIMER_PADDING;
   edtTitle.Width := dtpSet.Left - edtTitle.Left - TIMER_PADDING;
+
+  // Vertically centre controls
+  cbSelect.Top := (Height - cbSelect.Height) div 2;
+  imgTimer.Top := (Height - imgTimer.Height) div 2;
+  edtTitle.Top := (Height - edtTitle.Height) div 2;
+  dtpSet.Top := (Height - dtpSet.Height) div 2;
+  bbPlay.Top := (Height - bbPlay.Height) div 2;
+  bbPause.Top := (Height - bbPause.Height) div 2;
+  bbStop.Top := (Height - bbStop.Height) div 2;
+  bbAdjust.Top := (Height - bbAdjust.Height) div 2;
+  lblCountdown.Top := (Height - lblCountdown.Height) div 2;
+  ckbIconProgress.Top := (Height - ckbIconProgress.Height) div 2;
+  bbEdit.Top := (Height - bbEdit.Height) div 2;
+
 end;
 
 function TfraTimer.RestartFromLastFinish: boolean;
